03d20a790de567772d753ce998ebf1b63d6036ff
rpnX <55102634+rpnX@users.noreply.github.com>
2019-11-19 15:00:54 +0300

    www

---
+++ Applebox/js/var.js
@@ -0,0 +1,22 @@
+const Показать = (num) => {
+    if (isNaN(num) == false) {
+    console.log('Яблок в коробке ' + num + ' штук')
+}
+    else {
+        console.log(num)
+    }
+}
+
+
+let AppleBox
+ = 9
+
+let AppleBox2 = 5
+ 
+AppleBox2 = 5 - 1
+let name = 'Павел'
+Показать(AppleBox)
+Показать(name)
+
+
+
